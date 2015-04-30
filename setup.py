35c33c313ca5ddd7c09177b0ac4b4b834f86a5ab
CJ Carey <perimosocordiae@gmail.com>
2015-04-30 16:53:32 -0400

    Scipy isn't a required dependency

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ setup(
     packages=['viztricks'],
     install_requires=[
         'numpy >= 1.6.1',
-        'scipy >= 0.10',
         'matplotlib >= 1.3.1',
     ],
     extras_require=dict(
         pca_ellipse=['scikit-learn >= 0.14'],
+        voronoi_filled=['scipy >= 0.10'],
     ),
 )
