033ca47107bf55c274ee11de5263202247a93c44
CJ Carey <perimosocordiae@gmail.com>
2015-04-30 13:54:35 -0400

    Fixing __all__ for the top-level package

--- viztricks/__init__.py
+++ viztricks/__init__.py
@@ -4,7 +4,7 @@ from shims import *
 from extensions import *
 
 __all__ = [
-    'plot', 'plot_trajectories', 'gradient_line', 'violinplot', 'quiver3d',
+    'plot', 'plot_trajectories', 'gradient_line', 'violinplot',
     'vector_field', 'FigureSaver', 'irregular_contour', 'voronoi_filled',
     'pca_ellipse', 'axes_grid', 'imagesc', 'embedded_images'
 ]
